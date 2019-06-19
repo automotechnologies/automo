7ca253b246bdbe768c80cb0586564ba1b30f98ad
Imanalopher <iman.mamadiyev@gmail.com>
2019-06-19 19:40:57 +0600

    Sitemap (Listing, Home page)

---
+++ src/Cocorico/CMSBundle/Controller/Frontend/SiteMapController.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace Cocorico\CMSBundle\Controller\Frontend;
+
+use Cocorico\CoreBundle\Entity\ListingImage;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * Class SiteMapController
+ * @package Cocorico\CMSBundle\Controller\Frontend
+ *
+ */
+class SiteMapController extends Controller
+{
+    /**
+     * @return Response
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $urls = [];
+
+        // add static urls
+        $urls[] = ['loc' => $this->generateUrl('cocorico_home', [], UrlGeneratorInterface::ABSOLUTE_URL)];
+
+        $listings = $em->getRepository('CocoricoCoreBundle:Listing')->findAll();
+
+        foreach ($listings as $listing) {
+
+            /** @var $listingImage ListingImage */
+            $listingImage = $listing->getImages()->get(0);
+            $url = $this->container->get('liip_imagine.cache.manager')->getBrowserPath($listingImage->getWebPath(), 'listing_large');
+
+            $urls[] = [
+                'loc' => $this->get('router')->generate(
+                    'cocorico_listing_show',
+                    ['slug' => $listing->getSlug()],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                ),
+                'priority' => '0.7',
+                'lastmod' => $listing->getUpdatedAt(),
+                'changefreq' => 'weekly',
+                'image' => [
+                    'loc' => $url,
+                    'title' => $listing->getTitle(),
+                ]
+            ];
+        }
+
+        return $this->render('@CocoricoCMS/Frontend/sitemap.xml.twig', ['urls' => $urls]);
+    }
+}
