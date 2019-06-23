b6fe8aff35582df34618e514eb91bc0b785bb312
Imanalopher <iman.mamadiyev@gmail.com>
2019-06-23 12:44:11 +0600

    Blog news in cache and for it created service

--- src/Cocorico/CoreBundle/Controller/Frontend/HomeController.php
+++ src/Cocorico/CoreBundle/Controller/Frontend/HomeController.php
@@ -54,52 +54,8 @@ class HomeController extends Controller
     private function getBlogNewsFromCache()
     {
         $blogNews = $this->get('cache.app')->getItem('blog-news');
-        $feed = $this->getParameter('cocorico.home_rss_feed');
-        $renderFeeds = [];
-        $cacheTime = 3600 * 12;
 
-        if ($blogNews->isHit()) {
-            $renderFeeds = $blogNews->get();
-        } else {
-            $content = @file_get_contents($feed);
-            $feeds = [];
-
-            if ($content) {
-                try {
-                    $feeds = new \SimpleXMLElement($content);
-                    $feeds = $feeds->channel->xpath('//item');
-                } catch (\Exception $e) {
-                    // silently fail error
-                }
-            }
-
-            $uploadsDir = $this->getParameter('kernel.project_dir') . "/web/uploads/blog-news";
-
-            /**
-             * @var                    $key
-             * @var  \SimpleXMLElement $feed
-             */
-            foreach ($feeds as $key => $feed) {
-                $mediaArray = $feed->xpath('media:thumbnail');
-                $media = end($mediaArray);
-                $renderFeeds[$key]['title'] = (string)$feed->children()->title;
-                $renderFeeds[$key]['pubDate'] = (string)$feed->children()->pubDate;
-                $renderFeeds[$key]['link'] = (string)$feed->children()->link;
-                $imageUrl = end($media->attributes()->url);
-                $pathInfo = pathinfo($imageUrl);
-                $imageName = uniqid() . "." . $pathInfo['extension'];
-                $imageLocal = $uploadsDir .'/'. $imageName;
-                file_put_contents($imageLocal, file_get_contents($imageUrl));
-                $renderFeeds[$key]['image'] = "/uploads/blog-news/" . $imageName;
-                if ($key === 4)
-                    break;
-            }
-            $blogNews->set($renderFeeds);
-            $blogNews->expiresAfter($cacheTime);
-            $this->container->get('cache.app')->save($blogNews);
-        }
-
-        return $renderFeeds;
+        return $blogNews->isHit() ? $blogNews->get() : $this->get('cocorico.blog_news')->getBlogNews();
     }
 
 }
